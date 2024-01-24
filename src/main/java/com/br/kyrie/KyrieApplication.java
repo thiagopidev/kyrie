bbe80a2125378621d02243fda4fe5ac23a049e5e
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-01-24 10:57:18 -0300

    Documentação da classe principal

--- src/main/java/com/br/kyrie/KyrieApplication.java
+++ src/main/java/com/br/kyrie/KyrieApplication.java
@@ -3,11 +3,13 @@ package com.br.kyrie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe principal da aplicação
+ * @author Thiago Pinheiro
+**/
 @SpringBootApplication
 public class KyrieApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KyrieApplication.class, args);
 	}
-
 }
