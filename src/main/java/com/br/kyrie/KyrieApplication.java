aecf87378622a81959a14886c5cbc562d0b5a0c0
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-01-24 10:48:46 -0300

    Início do projeto

---
+++ src/main/java/com/br/kyrie/KyrieApplication.java
@@ -0,0 +1,13 @@
+package com.br.kyrie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyrieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyrieApplication.class, args);
+	}
+
+}
