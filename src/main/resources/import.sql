dc24a76e33827c2cd0639299abab7cee500af9e7
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-01-27 11:57:08 -0300

    Importação de usuário, perfil e autorização iniciais

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO roles (authority) VALUES ("ADMIN")
+INSERT INTO users (password, username) VALUES ("$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", "thiagopinheiro.nascimento@gmail.com")
+INSERT INTO authorities VALUES (1,1)
