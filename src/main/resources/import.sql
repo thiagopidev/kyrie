8df4da3bef9f4e7f024ca1d64fbdd1b26765bccc
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-02-05 10:51:18 -0300

    Importação inicial de batismo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
 INSERT INTO roles (authority) VALUES ("ADMIN")
 INSERT INTO users (password, username) VALUES ("$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", "thiagopinheiro.nascimento@gmail.com")
 INSERT INTO authorities VALUES (1,1)
+
+INSERT INTO baptisms (description, date, created_at, updated_at) VALUES ("PRIMEIRO BATIZADO DE 2024", "2024-05-22", utc_timestamp, utc_timestamp)
