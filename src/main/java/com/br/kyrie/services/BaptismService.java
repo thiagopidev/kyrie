1764779d62b12b7e674de49ae900cd077e9405e1
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-02-05 11:07:11 -0300

    Classe de serviço de batismo

---
+++ src/main/java/com/br/kyrie/services/BaptismService.java
@@ -0,0 +1,42 @@
+package com.br.kyrie.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.kyrie.exceptions.EntityNotFoundException;
+import com.br.kyrie.exceptions.EntityNotPersistedException;
+import com.br.kyrie.models.Baptism;
+import com.br.kyrie.repositories.BaptismRepository;
+
+import jakarta.transaction.Transactional;
+
+/**
+ * Classe de serviço de batismo
+ * @author Thiago Pinheiro
+**/
+@Service
+public class BaptismService {
+	
+	@Autowired
+	private BaptismRepository baptismRepository;
+	
+	@Transactional
+	public void persist(Baptism baptism) {
+		if(baptismRepository.save(baptism) == null)
+			throw new EntityNotPersistedException("Erro ao salvar batismo");
+	}
+	
+	public Baptism getById(Long id) {
+		Optional<Baptism> baptism = baptismRepository.findById(id);
+		if(baptism.isEmpty())
+			throw new EntityNotFoundException("Batismo não encontrado");
+		return baptism.get();
+	}
+	
+	@Transactional
+	public void remove (Baptism baptism) {
+		baptismRepository.deleteById(baptism.getId());
+	}
+}
