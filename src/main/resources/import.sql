8c1a22f7f75cce689350d5b6320ee42fd821689d
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-02-16 10:19:05 -0300

    Adição de created_at e updated_at na inserção inicial de batismo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,4 @@
 INSERT INTO roles (authority) VALUES ("ADMIN")
 INSERT INTO users (password, username) VALUES ("$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", "thiagopinheiro.nascimento@gmail.com")
 INSERT INTO authorities VALUES (1,1)
-
-INSERT INTO baptisms (description, date, created_at, updated_at) VALUES ("PRIMEIRO BATIZADO DE 2024", "2024-05-22", utc_timestamp, utc_timestamp)
+INSERT INTO baptisms (description, date, created_at, updated_at, created_by, updated_by) VALUES ("PRIMEIRO BATIZADO DE 2024", "2024-05-22", utc_timestamp, utc_timestamp, "thiagopinheiro.nascimento@gmail.com", "thiagopinheiro.nascimento@gmail.com")
