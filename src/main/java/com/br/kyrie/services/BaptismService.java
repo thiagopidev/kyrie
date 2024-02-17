01197d794e70c1726adda4f19c768ab20503b5dd
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-02-17 13:19:51 -0300

    Validação de batismo igual ou inferior a data de hoje

--- src/main/java/com/br/kyrie/services/BaptismService.java
+++ src/main/java/com/br/kyrie/services/BaptismService.java
@@ -1,11 +1,13 @@
 package com.br.kyrie.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.br.kyrie.exceptions.EntityAlreadyPassedException;
 import com.br.kyrie.exceptions.EntityNotFoundException;
 import com.br.kyrie.exceptions.EntityNotPersistedException;
 import com.br.kyrie.models.Baptism;
@@ -22,7 +24,9 @@ public class BaptismService {
 	private BaptismRepository baptismRepository;
 	
 	@Transactional
-	public void persist(Baptism baptism) {
+	public void persist(Baptism baptism) throws EntityAlreadyPassedException {
+		if(baptism.getDate().before(new Date()))
+			throw new EntityAlreadyPassedException("A data do batismo não pode ser igual ou inferior a data de hoje");
 		if(baptismRepository.save(baptism) == null)
 			throw new EntityNotPersistedException("Erro ao salvar batismo");
 	}
